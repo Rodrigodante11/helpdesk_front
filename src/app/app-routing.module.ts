7d2cfeddb2993522fbbbbe91fb51f32e70b4e68f
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2024-03-09 11:55:26 -0300

    criando rotas no sistema

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,8 +25,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { BrowserModule } from '@angular/platform-browser';
+import {NavComponent} from "./components/nav/nav.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: NavComponent
+    }
+];
 
 @NgModule({
   imports: [
