262ae993f8bf187550a3f377adcb133edffc1f25
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2024-03-14 17:58:22 -0300

    chamado model e service

---
+++ src/app/services/chamado.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Chamado} from "../../models/chamados";
+import {API_CONFIG} from "../config/api.config";
+import {templateJitUrl} from "@angular/compiler";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChamadoService {
+
+  constructor(private http: HttpClient ) { }
+  
+  findAll(): Observable<Chamado[]> {
+    return this.http.get<Chamado[]>(`${API_CONFIG.baseUrl}/chamados`)
+  }
+
+  findById(id: any): Observable<Chamado>{
+    return this.http.get<Chamado>(`${API_CONFIG.baseUrl}/chamados/${id}`)
+  }
+
+  create(chamado: Chamado): Observable<Chamado>{
+    return this.http.post<Chamado>(`${API_CONFIG.baseUrl}/chamados`, chamado)
+  }
+
+  update(chamado: Chamado): Observable<Chamado>{
+    return this.http.put<Chamado>(`${API_CONFIG.baseUrl}/chamados/${chamado.id}`, chamado)
+  }
+}
